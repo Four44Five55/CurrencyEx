416cee2d137e46cb022fa002dadd138d1ca93243
Four44Five55 <Four4Five55@yandex.ru>
2025-07-22 23:53:43 +0300

    -feat добавлена БД и класс модель для валюты

---
+++ src/main/java/org/example/model/Currency.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+public class Currency {
+    private int id;
+    private String charCode;
+    private int numCode;
+    private String fullName;
+    private String sign;
+
+    public Currency() {
+    }
+
+    public Currency(int id, String charCode, int numCode, String fullName, String sign) {
+    }
+}
+
